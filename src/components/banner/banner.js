f519d5a985b86744ede0a33e0c9fa0a92ee6fa46
Kseniia Anishchenko <kseninavoi@gmail.com>
2020-08-29 15:15:57 -0700

    added hooks

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,36 +1,25 @@
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
 
 import { FaTrophy } from "react-icons/fa";
 
 import "./banner.scss";
 
-class Banner extends Component {
-  constructor(props) {
-    super(props);
+const Banner = () => {
+  const [isVisible, setIsVisible] = useState('open');
 
-    this.state = {
-      isVisible: 'open'
-    }
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     window.scrollTo(0, 0);
-  }
+  });
 
-  render() {
-    const {isVisible} = this.state;
-    return <div className={`banner ${isVisible}`} >
-        <div className="banner__inner">
-          <FaTrophy />
-          <h3 className="title title--h3">Thank you for your choise</h3>
-          <button type="button" className="banner__btn" onClick={() => {
-            this.setState({
-              isVisible: false
-            })
-          }}>Close</button>
-        </div>
+  return <div className={`banner ${isVisible}`} >
+      <div className="banner__inner">
+        <FaTrophy />
+        <h3 className="title title--h3">Thank you for your choise</h3>
+        <button type="button" className="banner__btn" onClick={() => {
+          setIsVisible('');
+        }}>Close</button>
       </div>
-  }
+    </div>
 }
 
 export default Banner;
--- src/components/search-form/search-form.js
+++ src/components/search-form/search-form.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 
 import { FaSearch } from "react-icons/fa";
@@ -7,37 +7,23 @@ import {getMoviesByTitle} from "../../redux/movie/movie-reducer";
 
 import "./search-form.scss";
 
-class SearchForm extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchTitle: '',
-      moviesList: {}
-    }
-  }
-
-  render() {
-    const {searchTitle} = this.state;
-    const {getMoviesBySearchTitle} = this.props;
-    return <form className="search-form" action="#" method="post" onSubmit = {(evt) => {
-      evt.preventDefault();
-      getMoviesBySearchTitle(searchTitle);
-    }}>
-      <div className="search-form__input-wrap">
-        <label className="visually-hidden">Search movie</label>
-        <input className="search-form__input" type="text" name="movie" placeholder="Movie title" onChange={(evt) => {
-          const target = evt.target;
-          const value = target.value;
-          this.setState({
-            searchTitle: value
-          });
-        }}/>
-      </div>
-      <button className="search-form__submit" type="submit"><FaSearch /></button>
-    </form>
-  }
+const SearchForm = (props) => {
+  const [searchTitle, setSearchTitle] = useState('');
+  const {getMoviesBySearchTitle} = props;
+  return <form className="search-form" action="#" method="post" onSubmit = {(evt) => {
+    evt.preventDefault();
+    getMoviesBySearchTitle(searchTitle);
+  }}>
+    <div className="search-form__input-wrap">
+      <label className="visually-hidden">Search movie</label>
+      <input className="search-form__input" type="text" name="movie" placeholder="Movie title" onChange={(evt) => {
+        const target = evt.target;
+        const value = target.value;
+        setSearchTitle(value);
+      }}/>
+    </div>
+    <button className="search-form__submit" type="submit"><FaSearch /></button>
+  </form>
 }
 
 SearchForm.propTypes = {
