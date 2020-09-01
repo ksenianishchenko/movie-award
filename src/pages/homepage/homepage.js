9817c8e9d656e25313df2c877ff93961b8500394
Kseniia Anishchenko <kseninavoi@gmail.com>
2020-08-31 23:03:16 -0700

    added logic to homepage if user loggedin

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -4,28 +4,35 @@ import SearchForm from "../../components/search-form/search-form";
 import SearchResults from "../../components/search-results/serach-results";
 import NominateList from "../../components/nominate-list/nominate-list";
 import { Link } from 'react-router-dom';
+import {connect} from "react-redux";
 
 import "./homepage.scss";
 
-const Homepage = () => {
+const Homepage = (props) => {
+  const {isLoggedIn} = props;
   return <div className="homepage">
       <div className="homepage__container">
         <div className="homepage__inner homepage__inner--top">
           <h1 className="title title--h1">Movie Awards</h1>
           <p className="main-text">Find and save 5 your favourite films you feel should be up for nomination</p>
-          <Link to="/signup" className="homepage__link transition">Start here</Link>
-          <SearchForm />
+          {
+            isLoggedIn ? <SearchForm /> : <Link to="/signup" className="homepage__link transition">Start here</Link>
+          }
         </div>
       </div>
-      <div className="homepage__inner">
-        <div className="homepage__left">
-          <SearchResults />
+        <div className="homepage__inner">
+          <div className="homepage__left">
+            <SearchResults />
+          </div>
+          <div className="homepage__right">
+            <NominateList />
+          </div>
         </div>
-        <div className="homepage__right">
-          <NominateList />
-        </div>
-      </div>
     </div>
 }
 
-export default Homepage;
+const mapStateToProps = (state) => ({
+  isLoggedIn: state.user.isLoggedIn
+})
+
+export default connect(mapStateToProps, null)(Homepage);
